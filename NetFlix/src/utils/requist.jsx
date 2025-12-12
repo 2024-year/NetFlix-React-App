2edebc0c8f2155363d3d16f9e3bb2da1b5aafd93
ferehan <ahmedferehan7@gmail.com>
2025-12-13 02:21:11 +0300

    Data Fetching

---
+++ NetFlix/src/utils/requist.jsx
@@ -0,0 +1,7 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const requist={
+    fetchTrending:`/discover/movie?api_key=${API_KEY}&language=en-US&page=1&sort_by=popularity.desc`,
+    fetchTv:`/discover/tv?${API_KEY}&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc`
+    
+};
+export default requist;
